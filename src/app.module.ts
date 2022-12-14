db65ab8e11b1a3181811ef54fe9596d08e0d8223
wangxiaosong <wangxiaosong1234@hotmail.com>
2022-12-14 16:56:58 +0800

    -feat: modules 目录文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { MainGuard } from './app.mainGuard';
 import { CatsModule } from './modules/cats/index';
+import { GetMoudels } from './modules';
+let _GetMoudels = new GetMoudels();
 
 @Module({
-  imports: [CatsModule],
+  imports: _GetMoudels.getList(),
   controllers: [],
   providers: [
     {
---
+++ src/modules/index.ts
@@ -0,0 +1,9 @@
+import { CatsModule } from './cats';
+
+let list = [CatsModule];
+
+export class GetMoudels {
+  public getList() {
+    return list;
+  }
+}
